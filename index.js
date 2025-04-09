1dd3f119750b8e555b35f2b36357abdcf20f7d4f
Safaizal <127269918+Safaizal@users.noreply.github.com>
2025-04-09 10:18:03 +0530

    I have implemented the .FromTo() and Set(), It's getting interesting, Tweening is fun...

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 let box = document.querySelector(".box");
-let circle = DocumentFragment.querySelector(".circle");
+let circle = document.querySelector(".circle");
+let rec = document.querySelector(".rectangle");
 
-gsap.to([box, circle],
+gsap.to(box,
     {
         x: 200,
         rotation: 360,
         duration: 5
     }
 )
+
+gsap.fromTo(circle, { x: 0, y:0 }, { x: 200, y:200, duration: 5 });
+
+gsap.set(rec, { x:175, y:0})
