e9cc80d2580a6e454bf213a6c7cab582cf2fe3b8
Safaizal <127269918+Safaizal@users.noreply.github.com>
2025-04-09 14:53:55 +0530

    I have learned about the Special property of gsap, repeat, ease, delay, yoyo. Ease is one of the coolest thing i have seen in my web tech journey.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const label = document.querySelector(".label");
 slider.addEventListener('input', function() {
     gsap.to(progress, {
         width: `${this.value}%`,
-        duration: 0.5,
+        duration: 0.2,
         onUpdate:  function(){
             label.textContent = `${Math.round(gsap.getProperty(progress , "width"))}%`;
         }
@@ -18,12 +18,44 @@ slider.addEventListener('input', function() {
 
 gsap.to(box,
     {
-        x: 200,
+        y: 200,
         rotation: 360,
-        duration: 5
+        duration: 2.5,
+        yoyo: true,
+        // repeat:-1
     }
 )
+gsap.to(circle,
+    {
+    x: 0,
+    y: 200,
+    repeat: -1,
+    yoyo:true, 
+    duration: 2.5,
+    ease: "bounce.out"
+}
+)
 
-gsap.fromTo(circle, { x: 0, y:0 }, { x: 200, y:200, duration: 5 });
+gsap.to(rec, {
+    x: 200,
+    y: -200,
+    rotation: 360,
+    // repeat: -1,
+    yoyo: true,
+    duration: 2.5
+})
 
-gsap.set(rec, { x:0, y:0})
+let bs = document.querySelector(".big_square");
+
+gsap.to(bs, {
+    x:200,
+    // repeat:-1, This will repeat the animation infinitely
+    repeat: -1,
+    duration: 2.5,
+    //delay: 2, It is for delaying the animation by seconds
+    yoyo: true,
+    // onComplete: () =>{
+    //     alert("Animation completed!"); It will only work when the repeat is defined to finite.
+    // }
+    ease: "elastic.out"
+})
