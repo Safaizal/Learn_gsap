a8e6b70ea60f937d64bea7cf022dba9566897b6b
Safaizal <127269918+Safaizal@users.noreply.github.com>
2025-04-14 16:05:22 +0530

    I have learned about the stagger property which is really cool when it comes to many object animation, and properties like amount, each int stagger, then property like lazy, i don't get what actually that lazy property do. need to study docs

--- index.js
+++ index.js
@@ -1,61 +1,26 @@
-let box = document.querySelector(".box");
-let circle = document.querySelector(".circle");
-let rec = document.querySelector(".rectangle");
+let container = document.querySelector('.container');
 
-const slider = document.querySelector(".slider");
-const progress = document.querySelector(".progress");
-const label = document.querySelector(".label");
-
-slider.addEventListener('input', function() {
-    gsap.to(progress, {
-        width: `${this.value}%`,
-        duration: 0.2,
-        onUpdate:  function(){
-            label.textContent = `${Math.round(gsap.getProperty(progress , "width"))}%`;
-        }
-    });
-});
-
-gsap.to(box,
-    {
-        y: 200,
-        rotation: 360,
-        duration: 2.5,
-        yoyo: true,
-        // repeat:-1
-    }
-)
-gsap.to(circle,
-    {
-    x: 0,
-    y: 200,
-    repeat: -1,
-    yoyo:true, 
-    duration: 2.5,
-    ease: "bounce.out"
+function createcircle(){
+    let circle = document.createElement('div');
+    circle.classList.add('circle');
+    container.appendChild(circle);
 }
-)
 
-gsap.to(rec, {
-    x: 200,
-    y: -200,
-    rotation: 360,
-    // repeat: -1,
-    yoyo: true,
-    duration: 2.5
-})
+for(let i = 0; i < 100; i++){
+    createcircle();
+}
 
-let bs = document.querySelector(".big_square");
-
-gsap.to(bs, {
-    x:200,
-    // repeat:-1, This will repeat the animation infinitely
-    repeat: -1,
-    duration: 2.5,
-    //delay: 2, It is for delaying the animation by seconds
-    yoyo: true,
-    // onComplete: () =>{
-    //     alert("Animation completed!"); It will only work when the repeat is defined to finite.
-    // }
-    ease: "elastic.out"
+gsap.to('.circle', {
+    scale: 0.1,
+    y: 40,
+    duration: 0.5,
+    opacity: 0,
+    lazy: true,
+    stagger: {
+        amount: 0.5, 
+        from: "edges",
+        grid: [10, 10],
+        yoyo: true,
+        repeat: -1
+    }
 })
